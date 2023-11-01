2f4a07d43bb62b53b957498b54aee3f386f46a36
cybermavka <pavlenko.maksym.a@gmail.com>
2023-11-01 23:27:05 +0200

    testing new form

--- Frontend_PC/main.cpp
+++ Frontend_PC/main.cpp
@@ -1,3 +1,4 @@
+#include <QtWidgets>
 #include <QApplication>
 #include <QDialog>
 #include <QVBoxLayout>
@@ -6,16 +7,53 @@
 #include <QLabel>
 #include <QMessageBox>
 
-#include "auth_window.h"
-#include "register_window.h"
+
+
+
+class TrainingTestForm : public QWidget {
+    Q_OBJECT
+
+public:
+    TrainingTestForm(QWidget *parent = nullptr) : QWidget(parent) {
+        QVBoxLayout *layout = new QVBoxLayout;
+        QLabel *questionLabel = new QLabel("Question:");
+        QTextEdit *questionTextEdit = new QTextEdit;
+
+        QLabel *answersLabel = new QLabel("Answers:");
+        QLineEdit *answer1 = new QLineEdit;
+        QLineEdit *answer2 = new QLineEdit;
+        QLineEdit *answer3 = new QLineEdit;
+        QLineEdit *answer4 = new QLineEdit;
+
+        QPushButton *addMediaButton = new QPushButton("Add Media");
+
+        layout->addWidget(questionLabel);
+        layout->addWidget(questionTextEdit);
+        layout->addWidget(answersLabel);
+        layout->addWidget(answer1);
+        layout->addWidget(answer2);
+        layout->addWidget(answer3);
+        layout->addWidget(answer4);
+        layout->addWidget(addMediaButton);
+
+        connect(addMediaButton, &QPushButton::clicked, this, &TrainingTestForm::addMedia);
+
+        setLayout(layout);
+    }
+
+private slots:
+    void addMedia() {
+        QString filePath = QFileDialog::getOpenFileName(this, "Select Media File", QDir::homePath(), "Images (*.png *.jpg *.jpeg);;Videos (*.mp4 *.avi)");
+        // Store the media file path for later use.
+        // You can handle this data as needed.
+    }
+};
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    AuthorizationDialog dialog;
-    //RegistrationDialog dialog;
-
-    //dialog.setWindowTitle("User Registration Form");
-    dialog.show();
-
+    TrainingTestForm window;
+    window.show();
     return app.exec();
 }
+
+#include "main.moc"

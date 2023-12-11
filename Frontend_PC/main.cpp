cae345ae81ebc652820f5cf5c1b7645af29d75b2
Mavka <pavlenko.maksym.a@gmail.com>
2023-12-11 23:13:12 +0200

    Update main.cpp

--- Frontend_PC/main.cpp
+++ Frontend_PC/main.cpp
@@ -1,11 +1,5 @@
 #include <QtWidgets>
 #include <QApplication>
-#include <QDialog>
-#include <QVBoxLayout>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QLabel>
-#include <QMessageBox>
 
 int main(int argc, char *argv[]) {
     return 0;
